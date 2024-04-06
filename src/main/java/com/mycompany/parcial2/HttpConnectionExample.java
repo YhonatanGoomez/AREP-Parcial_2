b0d008b28e3a86569244609efac8f6c1d1d87822
YhonatanGoomez <90011314+YhonatanGoomez@users.noreply.github.com>
2024-04-06 04:44:12 -0500

    Test/Imagenes

--- src/main/java/com/mycompany/parcial2/HttpConnectionExample.java
+++ src/main/java/com/mycompany/parcial2/HttpConnectionExample.java
@@ -11,7 +11,6 @@ package com.mycompany.parcial2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
